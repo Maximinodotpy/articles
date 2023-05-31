d74d0f5f235d164330837f2e8e1998f54bd3fd94
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2023-05-31 19:19:56 +0200

    Update create.py

--- 039 - Keyboard Shortcuts/create.py
+++ 039 - Keyboard Shortcuts/create.py
@@ -7,9 +7,6 @@ print('Compiling Shortcuts to Markdown Article ...')
 shortcutData = json.load(open('shortcuts.json', 'r'))['applications']
 shortcutBlacklist = json.load(open('quiz-blacklist.json', 'r'))
 
-os.system('cd quiz && npm run build')
-quizHTMLContent = open('quiz/dist/index.html', 'r').read()
-
 result = """---
 name: 'Keyboard Shortcuts'
 slug: 'keyboard-shortcuts'
