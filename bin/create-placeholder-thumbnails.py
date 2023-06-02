05458fb68f73c25d86305dae39240b68c0b28190
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2023-06-02 12:58:41 +0200

    c

---
+++ bin/create-placeholder-thumbnails.py
@@ -0,0 +1,52 @@
+import os
+import glob
+import pathlib as pl
+import cairo
+
+print("Creating placeholder images...")
+
+foundMarkdownFiles = glob.glob('**/_blog/article.md', recursive=True)
+
+for file in foundMarkdownFiles:
+    articleDirectory = pl.Path(file).parent
+    potentialThumbnailPath = articleDirectory / pl.Path('preview.png')
+
+    if (not os.path.exists(potentialThumbnailPath)):
+        print("No thumbnail found for " + str(articleDirectory))
+
+        surface = cairo.SVGSurface('bin/example.svg', 1000, 500)
+
+        # creating a cairo context object for SVG surface
+        # using Context method
+        context = cairo.Context(surface)
+    
+
+
+        # Creating a liner gradient object.
+        lg1 = cairo.LinearGradient(0.0, 0.0, 0, 500)
+        lg1.add_color_stop_rgba(0, 0, 0, 0, 1)
+        lg1.add_color_stop_rgba(1, 0.5, 0.5, 1, 1)
+    
+        # Creating Shape
+        context.rectangle(0, 0, 1000, 500)
+        context.set_source(lg1)
+        # Fill the color inside the rectangle
+        context.fill()
+
+        # ölkafsdj
+        context.set_font_size(50)
+        context.set_source_rgb(1, 1, 1)
+      
+        # Font Style
+        context.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+        
+        # position for the text
+        context.move_to(50, 200)
+        
+        # displays the text
+        context.show_text("GeeksForGeeks")
+        
+        # stroke out the color and width property
+        context.stroke()
+
+        surface.write_to_png('bin/example.png')
