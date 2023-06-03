77b5972ee3a9bf9085d73bca532a9fa063a3c3f1
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2023-06-03 09:55:35 +0200

    c

--- 040 - Line Chart with Svelte/_blog/article.md
+++ 040 - Line Chart with Svelte/_blog/article.md
@@ -8,8 +8,18 @@ status: 'draft'
 ---
 
 
-
+<!-- Introduction -->
 
 <!-- 
 - Array(30).fill(1).map(x => Math.random() * 100 + 10)
+- SVG
 -->
+
+
+## Line Chart Component
+
+## Using the Line Chart Component
+
+## Conclusion
+
+
--- bin/update-readme.py
+++ bin/update-readme.py
@@ -4,14 +4,14 @@ import requests
 defaultText = """
 ![Maximmaeder.com Logo](https://maximmaeder.com/wp-content/uploads/2022/06/blog-logo_v1-150x150.png)
 
+Hi, my name is Maxim Maeder, a young programming enthusiast looking to have fun coding and teaching you some things about programming.
+
 # Articles
 This repository holds the code for my Tutorials; feel free to copy folders or the whole storage to test and look at the code. Each Article has its folder; optionally, there is also a *_blog* folder that contains the article's markdown, preview images, and so on.
 
-[maximmaeder.com](https://maximmaeder.com/)
+[maximmaeder.com](https://maximmaeder.com/articles/)
 
-## Demos
-
-The demos of articles revolving around Web technologies can be found [here](https://demos.maximmaeder.com/). This page shows subdirectories in this folder, each representing an article's demo page.
+Please dont refrain from creating an Issue if you find an Error in one of my Articles or the Code.
 
 ## Article Links
 
@@ -48,5 +48,3 @@ for i in chunks(content['items'], columns):
 
 with open('README.md', 'w') as f:
     f.write(finalString)
-
-print(finalString)
