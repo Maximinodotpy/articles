d46759a99ff820b261b84d88c96f32070a800614
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2023-06-28 08:51:53 +0200

    Carousel

--- 042 - Carousel Component with Svelte/src/App.svelte
+++ 042 - Carousel Component with Svelte/src/App.svelte
@@ -1,7 +1,60 @@
-<script>
-  
+<script lang="ts">
+  import Carousel from "./lib/Carousel.svelte";
+  import { faker } from "@faker-js/faker";
+
 </script>
 
 <div>
   <h1>Carousel Component with Svelte</h1>
 </div>
+
+<h2>Unstyled</h2>
+
+<Carousel>
+  {#each Array(10) as _, i}
+    <div style="width: 100%">
+      <img src={faker.image.avatar() } alt="" width="50">
+      <h2>{ faker.person.fullName() }</h2>
+      <p>{faker.lorem.paragraph()}</p>
+      <p>@{ faker.company.name() }</p>
+    </div>
+  {/each}
+</Carousel>
+
+<h2>Styled</h2>
+
+<div class="custom_carousel">
+  <Carousel --cr-button-bg-color="hsl(0 0% 95%)">
+    {#each Array(10) as _, i}
+      <div style="" class="item">
+        <img src={faker.image.avatar() } alt="" width="100" style="flex-shrink: 0">
+        <div>
+          <h2>{ faker.person.fullName() }</h2>
+          <p>{faker.lorem.paragraph()}</p>
+          <p>@{ faker.company.name() }</p>
+        </div>
+      </div>
+    {/each}
+  </Carousel>
+</div>
+
+<style>
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .item {
+    font-family: monospace;
+    width: 100%;
+    display: flex;
+    gap: 2rem;
+    align-items: center;
+    padding-inline: 4rem;
+  }
+
+  .custom_carousel {
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+</style>
--- 042 - Carousel Component with Svelte/src/lib/Carousel.svelte
+++ 042 - Carousel Component with Svelte/src/lib/Carousel.svelte
@@ -1,4 +1,71 @@
-<script>
-    
+<script lang="ts">
+    let current_width = 0;
+    let container : HTMLElement;
+
+    let scroll_left = 0
+    let scroll_width = 0
+
+    function left() {
+        container.scrollBy(-current_width, 0)
+    }
+    function right() {
+        container.scrollBy(current_width, 0)
+    }
 </script>
 
+<div class="container">
+    <button on:pointerdown={left} style="opacity: {scroll_left == 0 ? '0' : '1'}">
+        <slot name="left_button">&lt;</slot>
+    </button>
+    <div
+        class="items-container"
+        bind:clientWidth={current_width}
+        bind:this={container}
+        on:scroll={() => { scroll_left = container.scrollLeft; scroll_width = container.scrollWidth;}}>
+        <slot></slot>
+    </div>
+    <button on:pointerdown={right} style="opacity: { (Math.abs((scroll_left + current_width) - scroll_width) < 3) ? '0' : '1' }">
+        <slot name="right_button">&gt;</slot>
+    </button>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        overflow: hidden;
+        gap: var(--cr-container-gap, 0.5rem);
+        justify-content: space-between;
+        width: 100%;
+    }
+
+    button {
+        color: var(--cr-button-color, #000);
+        background-color: var(--cr-button-bg-color, white);
+        border-color: var(--cr-button-border-color, white);
+        border-width: var(--cr-button-border-width, 1px);
+        border-radius: var(--cr-button-border-radius, 0.25rem);
+        border-style: var(--cr-button-border-style, none);
+        padding: var(--cr-button-padding, 1rem);
+    }
+
+    .items-container {
+        display: flex;
+        flex-grow: 1;
+        overflow: auto;
+
+        overflow-x: auto;
+        scroll-snap-type: x mandatory;
+
+        scroll-behavior: var(--cr-scroll-behavior, smooth);
+        -webkit-overflow-scrolling: touch;
+    }
+
+    :global(.items-container > *) {
+        scroll-snap-align: start;
+        flex-shrink: 0;
+    } 
+    
+    .items-container::-webkit-scrollbar {
+        display: none;
+    }
+</style>
